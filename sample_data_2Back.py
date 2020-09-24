63f0a468b7fa89b5c26d83f3bf656b7d13c97320
BagotLab <40178627+BagotLab@users.noreply.github.com>
2020-09-23 23:20:54 -0400

    Update sample_data_2Back.py

--- sample_data_2Back.py
+++ sample_data_2Back.py
@@ -255,7 +255,7 @@ def create_numpy_arrays():
             lsr2 = []
             
         for p in range(1001): #1000 samples per param combination
-            ah,rh,ws,ls,ws2,lse = generate_data_PRL([alpha, beta],100)
+            ah,rh,ws,ls,ws2,lse = generate_data_PRL([alpha, beta],100) #Change 100 to reflect trial number
             wsr.append(ws)
             lsr.append(ls)
             wsr2.append(ws2)
